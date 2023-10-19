05bb644ba0cf52b2a177d5db447994a836e818cd
Meenu <Meenu@LAPTOP-TOE98MBN>
2023-10-19 10:23:24 +0530

    commit 1st

---
+++ unwanted/src/rawan/ram.java
@@ -0,0 +1,16 @@
+package rawan;
+
+public class ram 
+{
+	public void m1()
+	{
+		System.out.println("this is m1 ");
+		
+	}
+public static void main(String[] args) 
+{
+	ram obj =new ram();
+	obj.m1();
+	
+}
+}
